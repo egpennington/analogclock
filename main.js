900a2cd4516fbadfefca634d7360ec6b8d540b0a
egpennington <egpennington@hotmail.com>
2024-01-01 00:38:37 -0600

    initial commit, clock set up

---
+++ main.js
@@ -0,0 +1,30 @@
+function show_clock(){
+    let h = document.getElementsByClassName('hr')[0]
+    let m = document.getElementsByClassName('mn')[0]
+    let s = document.getElementsByClassName('ss')[0]
+
+    let date = new Date()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+    let pe = "오전"
+
+    if(h === 0) {
+        h = 12
+      }
+      if(h > 12) {
+        h = h - 12
+        pe = "오후"
+      }
+
+    h.style.transform = `rotate(${30 * hours + minutes/2}deg)`;
+    m.style.transform = `rotate(${6 * minutes}deg)`;
+    s.style.transform = `rotate(${6 * seconds}deg)`;
+
+    period.innerHTML = pe;
+
+    let sound = new Audio('sound.mp3');
+    sound.play();
+}
+
+setInterval(show_clock, 1000);
