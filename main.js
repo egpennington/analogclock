e34ebea085cc7dd837324d4d2ce685130c1f4854
egpennington <egpennington@hotmail.com>
2024-01-01 16:37:42 -0600

    fixed am and pm display and added sitemanifest

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-function showClock(){
-    let h = document.getElementsByClassName('hr')[0]
-    let m = document.getElementsByClassName('mn')[0]
-    let s = document.getElementsByClassName('ss')[0]
+function showClock() {
+  let h = document.getElementsByClassName('hr')[0];
+  let m = document.getElementsByClassName('mn')[0];
+  let s = document.getElementsByClassName('ss')[0];
+  let period = document.getElementById('period');
 
-    let date = new Date()
-    let hours = date.getHours()
-    let minutes = date.getMinutes()
-    let seconds = date.getSeconds()
-    let pe = "오전"
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let pe = "오전";
 
-    if(h === 0) {
-        h = 12
-      }
-      if(h > 12) {
-        h = h - 12
-        pe = "오후"
-      }
+  if (hours === 0) {
+      h = 12;
+  }
+  if (hours > 12) {
+      hours = hours - 12;
+      pe = "오후";
+  }
 
-    h.style.transform = `rotate(${30 * hours + minutes/2}deg)`;
-    m.style.transform = `rotate(${6 * minutes}deg)`;
-    s.style.transform = `rotate(${6 * seconds}deg)`;
+  h.style.transform = `rotate(${30 * hours + minutes / 2}deg)`;
+  m.style.transform = `rotate(${6 * minutes}deg)`;
+  s.style.transform = `rotate(${6 * seconds}deg)`;
 
-    period.innerHTML = pe;
+  period.innerHTML = pe;
 
-    let sound = new Audio('sound.mp3');
-    sound.play();
+  // let sound = new Audio('sound.mp3');
+  // sound.play();
 }
 
 setInterval(showClock, 1000);
